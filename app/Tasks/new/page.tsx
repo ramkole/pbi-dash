f31249ba287d71451fbdf00ff2a095b7ab580a7e
ramkole <ram_kole@yahoo.com>
2023-11-08 14:09:06 +0530

    Error handling with zod in react hook form

--- app/Tasks/new/page.tsx
+++ app/Tasks/new/page.tsx
@@ -1,40 +1,63 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createPbiSchema } from "@/app/validationSchema";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/errorMessage";
 
-interface NewPbiForm {
-  title: string;
-  description: string;
-}
+type NewPbiForm = z.infer<typeof createPbiSchema>;
 
 const NewPbiPage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<NewPbiForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewPbiForm>({
+    resolver: zodResolver(createPbiSchema),
+  });
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/pbi", data);
-        router.push("/tasks");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/pbi", data);
+            router.push("/tasks");
+          } catch (error) {
+            setError("Unexpected error");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-      <Button>Create PBI</Button>
-    </form>
+        <Button>Create Task</Button>
+      </form>
+    </div>
   );
 };
 
