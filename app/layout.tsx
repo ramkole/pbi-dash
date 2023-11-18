b969dd6425056e2ff4ac309c63aed49edf09343f
ramkole <ram_kole@yahoo.com>
2023-11-18 17:23:20 +0530

    handled login logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Container, Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -24,12 +25,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="cyan">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="cyan">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
